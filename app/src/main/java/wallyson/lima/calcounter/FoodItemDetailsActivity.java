a32b9aaf6d518eb3957830753accacc145aa67f4
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-01-15 21:20:23 -0200

    Finished the user interface

---
+++ app/src/main/java/wallyson/lima/calcounter/FoodItemDetailsActivity.java
@@ -0,0 +1,13 @@
+package wallyson.lima.calcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodItemDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_item_details);
+    }
+}
