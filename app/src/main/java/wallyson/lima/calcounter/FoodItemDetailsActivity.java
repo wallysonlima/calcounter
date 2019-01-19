bda03e87ee8e26ffd5c1831a8ca35f98cdb79067
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-01-19 17:24:20 -0200

    Add delete functionality and finish the app

--- app/src/main/java/wallyson/lima/calcounter/FoodItemDetailsActivity.java
+++ app/src/main/java/wallyson/lima/calcounter/FoodItemDetailsActivity.java
@@ -1,8 +1,10 @@
 package wallyson.lima.calcounter;
 
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +12,12 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import data.DatabaseHandler;
 import model.Food;
 
 public class FoodItemDetailsActivity extends AppCompatActivity {
     private TextView foodName, calories, dateTaken;
-    private Button shareButton;
+    private Button shareButton, deleteButton;
     private int foodId;
 
     @Override
@@ -26,6 +29,7 @@ public class FoodItemDetailsActivity extends AppCompatActivity {
         calories = findViewById(R.id.detsCaloriesValue);
         dateTaken = findViewById(R.id.detsDateText);
         shareButton = findViewById(R.id.detsShareButton);
+        deleteButton = findViewById(R.id.detsDeleteButton);
 
         Food food = (Food) getIntent().getSerializableExtra("userObj");
 
@@ -41,6 +45,13 @@ public class FoodItemDetailsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 shareCals();
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteCals();
             }
         });
     }
@@ -66,5 +77,27 @@ public class FoodItemDetailsActivity extends AppCompatActivity {
         } catch(ActivityNotFoundException e) {
             Toast.makeText(getApplicationContext(), "Please install email client before ending", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void deleteCals() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(FoodItemDetailsActivity.this);
+        alert.setTitle("Delete ?");
+        alert.setMessage("Are you sure you want to delete this items ?");
+        alert.setNegativeButton("No", null);
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                DatabaseHandler dba = new DatabaseHandler(getApplicationContext());
+                dba.deleteFood(foodId);
+                Toast.makeText(getApplicationContext(), "Food Item Deleted with success !", Toast.LENGTH_LONG).show();
+
+                startActivity(new Intent(FoodItemDetailsActivity.this, DisplayFoodActivity.class));
+
+                // remove this activity from activity stack
+                FoodItemDetailsActivity.this.finish();
+            }
+        });
+
+        alert.show();
     }
 }
